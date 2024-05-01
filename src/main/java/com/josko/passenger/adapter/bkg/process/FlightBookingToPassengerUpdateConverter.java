2b702d687950d3f2bdaae6a56f5710133e1ea282
josko <jklo.jurce@gmail.com>
2024-05-01 22:23:15 +0200

    add pnr key; make ticket optional

--- src/main/java/com/josko/passenger/adapter/bkg/process/FlightBookingToPassengerUpdateConverter.java
+++ src/main/java/com/josko/passenger/adapter/bkg/process/FlightBookingToPassengerUpdateConverter.java
@@ -1,8 +1,11 @@
 package com.josko.passenger.adapter.bkg.process;
 
 import com.josko.passenger.adapter.bkg.dto.FlightBooking;
+import com.josko.passenger.adapter.bkg.dto.Passenger;
+import com.josko.passenger.adapter.bkg.dto.Ticket;
 import com.josko.passenger.update.dto.PassengerUpdate;
 import com.josko.passenger.update.dto.keys.KeyDTO;
+import com.josko.passenger.update.dto.keys.PnrKeyDTO;
 import com.josko.passenger.update.dto.keys.TicketNumberKeyDTO;
 import com.josko.passenger.update.slices.BookingData;
 import com.josko.passenger.update.slices.PassengerDetailsData;
@@ -17,6 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static com.josko.passenger.adapter.bkg.Definitions.DS_NAME;
+import static com.josko.passenger.update.dto.keys.KeyDTO.Type.PNR;
 import static com.josko.passenger.update.dto.keys.KeyDTO.Type.TICKET_NUMBER;
 import static com.josko.passenger.update.slices.SliceData.Type.BOOKING;
 import static com.josko.passenger.update.slices.SliceData.Type.DETAILS;
@@ -28,44 +32,37 @@ public class FlightBookingToPassengerUpdateConverter {
 
 	public static PassengerUpdate convert(FlightBooking bkg) {
 		PassengerUpdate passengerUpdate = new PassengerUpdate();
+		
 		passengerUpdate.setReference(UUID.randomUUID());
 		passengerUpdate.setDatasource(DS_NAME);
 		passengerUpdate.setUpdateTs(Instant.now());
-		
-		// Map keys
-		List<KeyDTO> keys = new ArrayList<>();
-		
-		TicketNumberKeyDTO ticketNumberKeyDTO = TicketNumberKeyDTO.builder()
-				.type(TICKET_NUMBER)
-				.carrier(bkg.getTicketDetails().getCarrier())
-				.flightNumber(bkg.getTicketDetails().getFlightNumber())
-				.departureDate(toLD(bkg.getTicketDetails().getDepartureDate()))
-				.ticketNumber(bkg.getTicketDetails().getTicketNumber())
-				.build();
-		
-		keys.add(ticketNumberKeyDTO);
-		passengerUpdate.setKeys(keys);
+		passengerUpdate.setKeys(getKeys(bkg));
+		passengerUpdate.setSlices(getSlices(bkg));
 
-		// Map slices
+		return passengerUpdate;
+	}
+
+	private static List<Slice> getSlices(FlightBooking bkg) {
 		List<Slice> slices = new ArrayList<>();
+
+		Passenger passenger = bkg.getPassenger();
 		
 		Slice<PassengerDetailsData> detailsSlice = Slice.<PassengerDetailsData>builder()
 				.name(DETAILS)
 				.content(PassengerDetailsData.builder()
-						.firstName(bkg.getPassenger().getFirstName())
-						.lastName(bkg.getPassenger().getLastName())
-						.title(bkg.getPassenger().getTitle())
-						.gender(bkg.getPassenger().getGender())
-						.dateOfBirth(toLD(bkg.getPassenger().getDateOfBirth()))
-						.city(bkg.getPassenger().getAddress().getCity())
-						.zipCode(bkg.getPassenger().getAddress().getZipCode())
-						.streetAndNumber(bkg.getPassenger().getAddress().getStreetAndNumber())
-						.country(bkg.getPassenger().getAddress().getCountry())
-						.language(bkg.getPassenger().getPreferredLanguage())
+						.firstName(passenger.getFirstName())
+						.lastName(passenger.getLastName())
+						.title(passenger.getTitle())
+						.gender(passenger.getGender())
+						.dateOfBirth(toLD(passenger.getDateOfBirth()))
+						.city(passenger.getAddress().getCity())
+						.zipCode(passenger.getAddress().getZipCode())
+						.streetAndNumber(passenger.getAddress().getStreetAndNumber())
+						.country(passenger.getAddress().getCountry())
+						.language(passenger.getPreferredLanguage())
 						.build())
 				.updateTs(Instant.now())
 				.build();
-		
 		slices.add(detailsSlice);
 
 		Slice<BookingData> bookingDataSlice = Slice.<BookingData>builder()
@@ -77,12 +74,37 @@ public class FlightBookingToPassengerUpdateConverter {
 						.build())
 				.updateTs(Instant.now())
 				.build();
-		
 		slices.add(bookingDataSlice);
 		
-		passengerUpdate.setSlices(slices);
+		return slices;
+	}
 
-		return passengerUpdate;
+	private static List<KeyDTO> getKeys(FlightBooking bkg) {
+		List<KeyDTO> keys = new ArrayList<>();
+
+		Ticket ticket = bkg.getTicketDetails();
+		Passenger passenger = bkg.getPassenger();
+		
+		if (ticket != null) {
+			var ticketNumberKey = TicketNumberKeyDTO.builder()
+					.type(TICKET_NUMBER)
+					.carrier(ticket.getCarrier())
+					.flightNumber(ticket.getFlightNumber())
+					.departureDate(toLD(ticket.getDepartureDate()))
+					.ticketNumber(ticket.getTicketNumber())
+					.build();
+			keys.add(ticketNumberKey);
+		}
+
+		var pnrKey = PnrKeyDTO.builder()
+				.type(PNR)
+				.firstName(passenger.getFirstName())
+				.lastName(passenger.getLastName())
+				.locator(bkg.getLocator())
+				.build();
+		keys.add(pnrKey);
+		
+		return keys;
 	}
 
 	private static LocalDate toLD(String dateString) {
